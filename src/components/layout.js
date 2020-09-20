080ca43d671e7e62884ff6acbbfb938d81c83437
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-21 01:07:15 +0500

    add layout and header

---
+++ src/components/layout.js
@@ -0,0 +1,80 @@
+import React, { Fragment } from "react"
+import { Global, css } from "@emotion/core"
+import Helmet from "react-helmet"
+import useSiteMetadata from "../hooks/use-sitemetadata"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+
+  return (
+    <Fragment>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+          }
+
+          > div {
+            margin-top: 0;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </Fragment>
+  )
+}
+
+export default Layout
