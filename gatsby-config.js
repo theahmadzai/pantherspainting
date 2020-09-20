9ff89464535b79e0c21ded32a8c5f5b280a3a241
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-09-21 01:06:39 +0500

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: ["gatsby-plugin-emotion"],
+  siteMetadata: {
+    title: "Panthers Painting",
+    description: "Panthers Painting Atlanta website description",
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
 }
