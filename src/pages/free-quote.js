ff3aabbb8e46b0f2a84ffce458d8e55cf53e4cac
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-02 02:20:55 +0500

    split to components

--- src/pages/free-quote.js
+++ src/pages/free-quote.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { Row, Col, Image } from 'antd'
+import { Row, Col } from 'antd'
 import Layout from '../components/Layout/Layout'
-import PageTitle from '../components/PageTitle/PageTitle'
+import QuoteInfo from '../components/QuoteInfo/QuoteInfo'
 import QuoteForm from '../components/QuoteForm/QuoteForm'
 
 export default () => (
   <Layout>
-    <PageTitle>Request a Free Quote</PageTitle>
     <Row>
-      <Col md={16}>
-        <QuoteForm />
+      <Col span={24} md={10}>
+        <QuoteInfo />
       </Col>
-      <Col md={8}>
-        <Image src="/postman.svg" />
+      <Col span={24} md={14}>
+        <QuoteForm />
       </Col>
     </Row>
   </Layout>
