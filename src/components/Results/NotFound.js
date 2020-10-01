47e28759b6ebd847422a445c7036ff489ae2cd09
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-02 00:22:23 +0500

    move

--- src/components/Results/NotFound.js
+++ src/components/Results/NotFound.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Result, Button } from 'antd'
 
-const NotFound = ({ message }) => (
+const NotFound = () => (
   <Result
     status="404"
     title="404"
-    subTitle={message || 'Sorry, the page you visited does not exist.'}
+    subTitle="Sorry, the page you visited does not exist."
     extra={
       <Button type="primary" onClick={() => window.history.back()}>
         Go Back
