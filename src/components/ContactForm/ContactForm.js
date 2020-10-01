19222a2b4b95f3ccf5487dc5c4bd7dcb4c6c0841
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-01 20:22:26 +0500

    update contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -14,37 +14,71 @@ const ContactForm = () => {
       size="large"
       name="contact"
       method="post"
+      scrollToFirstError
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      noValidate
     >
       <input type="hidden" name="form-name" value="contact" />
 
-      <Item label="Your Name" required>
+      <Item
+        label="Your Name"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: 'Please type your full name!',
+          },
+        ]}
+        required
+      >
         <Input
           type="text"
-          prefix={<UserOutlined className={styles.icon} />}
-          placeholder="John Doe"
           name="name"
+          placeholder="John Doe"
+          prefix={<UserOutlined className={styles.icon} />}
         />
       </Item>
-      <Item label="Phone" required>
-        <Input
-          type="tel"
-          prefix={<PhoneOutlined className={styles.icon} />}
-          placeholder="+123********"
-          name="phone"
-        />
-      </Item>
-      <Item label="Email">
+
+      <Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please type your email!',
+          },
+          {
+            type: 'email',
+            message: 'Please type a valid email!',
+          },
+        ]}
+        required
+      >
         <Input
           type="email"
-          prefix={<MailOutlined className={styles.icon} />}
-          placeholder="john@example.com"
           name="email"
+          placeholder="john@example.com"
+          prefix={<MailOutlined className={styles.icon} />}
         />
       </Item>
-      <Item label="Message">
-        <TextArea aplaceholder="Your message..." rows={5} name="message" />
+
+      <Item label="Phone" name="phone">
+        <Input
+          type="tel"
+          name="phone"
+          placeholder="+123********"
+          prefix={<PhoneOutlined className={styles.icon} />}
+        />
+      </Item>
+
+      <Item
+        label="Message"
+        name="message"
+        rules={[{ required: true, message: 'Please type a brief message!' }]}
+        required
+      >
+        <TextArea name="message" aplaceholder="Your message..." rows={5} />
       </Item>
 
       <Item>
