9e61665129dc924f5d9e2bc73a243af94739ebe6
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-02 22:48:19 +0500

    use paintbucket svg component

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Typography } from 'antd'
+import PaintBucket from '../Svgs/PaintBucket'
 import styles from './PageTitle.module.css'
 
 const { Title } = Typography
 
 const PageTitle = ({ children }) => (
-  <Title className={styles.title}>{children}</Title>
+  <Title className={styles.title}>
+    <PaintBucket />
+    <span className={styles.titleText}>{children}</span>
+  </Title>
 )
 
 export default PageTitle
