64709f8ed9bd8c278c71057ae0d3b7b8d75f2e76
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-03 15:38:00 +0500

    format

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -12,55 +12,53 @@ import styles from './Footer.module.css'
 
 const { Title, Paragraph } = Typography
 
-const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <Row gutter={24} className={styles.footerItems}>
-        <Col span={24} md={8} className={styles.footerItem}>
-          <Title level={3} className={styles.footerItemTitle}>
-            Address
-          </Title>
-          <Paragraph>Some Fake Address, Atlanta, Georgia</Paragraph>
-        </Col>
-        <Col span={24} md={8} className={styles.footerItem}>
-          <Title level={3} className={styles.footerItemTitle}>
-            Contact Info
-          </Title>
-          <Paragraph>
-            <PhoneOutlined title="Contact" /> +1 (0) 222X3214
-          </Paragraph>
-          <Paragraph>
-            <MailOutlined title="Email" />
-            &nbsp; info@pantherspainting.com
-          </Paragraph>
-        </Col>
-        <Col span={24} md={8} className={styles.footerItem}>
-          <Title level={3} className={styles.footerItemTitle}>
-            Get in Touch
-          </Title>
-          <Paragraph>
-            Subscribe to our RSS feed or follow us on Facebook and Twitter for
-            the latest content
-          </Paragraph>
-          <div style={{ fontSize: '2rem' }}>
-            <FacebookOutlined
-              title="Panthers Painting Facebook"
-              style={{ marginRight: '1rem' }}
-            />
-            <TwitterOutlined
-              title="Panthers Painting Twitter"
-              style={{ marginRight: '1rem' }}
-            />
-            <InstagramOutlined title="Panthers Painting Instagram" />
-          </div>
-        </Col>
-      </Row>
-      <div className={styles.copyright}>
-        <span>&copy; Copyright 2020 - Panthers Painting Atlanta</span>
-      </div>
-      <FooterCurve />
-    </footer>
-  )
-}
+const Footer = () => (
+  <footer className={styles.footer}>
+    <Row gutter={24} className={styles.footerItems}>
+      <Col span={24} md={8} className={styles.footerItem}>
+        <Title level={3} className={styles.footerItemTitle}>
+          Address
+        </Title>
+        <Paragraph>Some Fake Address, Atlanta, Georgia</Paragraph>
+      </Col>
+      <Col span={24} md={8} className={styles.footerItem}>
+        <Title level={3} className={styles.footerItemTitle}>
+          Contact Info
+        </Title>
+        <Paragraph>
+          <PhoneOutlined title="Contact" /> +1 (0) 222X3214
+        </Paragraph>
+        <Paragraph>
+          <MailOutlined title="Email" />
+          &nbsp; info@pantherspainting.com
+        </Paragraph>
+      </Col>
+      <Col span={24} md={8} className={styles.footerItem}>
+        <Title level={3} className={styles.footerItemTitle}>
+          Get in Touch
+        </Title>
+        <Paragraph>
+          Subscribe to our RSS feed or follow us on Facebook and Twitter for the
+          latest content
+        </Paragraph>
+        <div style={{ fontSize: '2rem' }}>
+          <FacebookOutlined
+            title="Panthers Painting Facebook"
+            style={{ marginRight: '1rem' }}
+          />
+          <TwitterOutlined
+            title="Panthers Painting Twitter"
+            style={{ marginRight: '1rem' }}
+          />
+          <InstagramOutlined title="Panthers Painting Instagram" />
+        </div>
+      </Col>
+    </Row>
+    <div className={styles.copyright}>
+      <span>&copy; Copyright 2020 - Panthers Painting Atlanta</span>
+    </div>
+    <FooterCurve />
+  </footer>
+)
 
 export default Footer
