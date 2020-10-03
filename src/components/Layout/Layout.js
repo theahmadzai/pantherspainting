bc92de098702865e5bd9383d2c872cb802c92487
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-03 23:15:08 +0500

    remove unused font

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,10 +15,6 @@ const Layout = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        ></link>
       </Helmet>
       <Header />
       <main className={styles.main}>{children}</main>
