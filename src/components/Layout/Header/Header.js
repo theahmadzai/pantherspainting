e74789700abaf3a31e5613bb82cc013e157d70c7
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-03 19:02:54 +0500

    use meta hook

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Image } from 'antd'
+import useSiteMetadata from '../../../hooks/use-sitemetadata'
 import Navbar from './Navbar/Navbar'
 import styles from './Header.module.css'
 
 const Header = () => {
+  const { title } = useSiteMetadata()
+
   return (
     <header className={styles.header}>
       <Link to="/">
-        <Image height={92} src="/logo.svg" alt="Panthers Painting" />
+        <Image height={92} src="/logo.svg" alt={title} />
       </Link>
 
       <Navbar />
