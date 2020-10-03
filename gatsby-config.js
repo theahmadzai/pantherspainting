99a5f3d73b297a895eb83a79e412bc356a31baa8
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-10-03 18:45:01 +0500

    add more details

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,20 @@ dotenv.config()
 
 module.exports = {
   siteMetadata: {
+    name: 'Panthers Painting',
     title: 'Panthers Painting',
     description: 'Panthers Painting Atlanta website description',
+    address: 'Some Fake Address, Atlanta, Georgia',
+    contacts: {
+      phone: '+1 (0) 222X3214',
+      email: 'info@pantherspainting.com',
+    },
+    social: {
+      facebook: 'https://www.facebook.com/pantherspainting',
+      twitter: 'https://www.twitter.com/@pantherspainting',
+      instagram: 'https://www.instagram.com/pantherspainting',
+    },
+    copyrights: 'Copyright 2020 - Panthers Painting Atlanta',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
