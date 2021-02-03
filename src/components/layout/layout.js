95ac6ab4a8b2afc94cbb42bb6cd815f716c83bab
Muhammad Javed <theahmadzai@hotmail.com>
2021-02-03 20:32:40 +0500

    update

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,20 +4,18 @@ import Header from './header'
 import Footer from './footer'
 import styles from './layout.module.less'
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <Helmet>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <Header />
-      <main className={styles.main}>{children}</main>
-      <Footer />
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <Header />
+    <main className={styles.main}>{children}</main>
+    <Footer />
+  </>
+)
 
 export default Layout
